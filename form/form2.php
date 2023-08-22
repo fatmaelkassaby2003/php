92cb0dd1d8ccad99090b370943357951ff4cd336
fatmaelkassaby2003 <125102138+fatmaelkassaby2003@users.noreply.github.com>
2023-08-22 21:14:13 +0300

    Add files via upload

---
+++ form/form2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/fcss2.css">
+    <title>FORM2</title>
+</head>
+<body>
+    <div class="A"><p>Sign Up</p></div>
+    <div class="C">
+        <div class="aa"><h2>First Name</h2><input type="text" placeholder="First Name"></div>
+        <div class="bb"><h2>Last Name</h2><input type="text" placeholder="Last Name"></div>
+        <div class="cc"><h2> Screen Name</h2><input type="text" placeholder="Enter Screen Name"></div>
+        <div class="dd"><h2>Email</h2><input type="text" placeholder="Enter Email"></div>
+        <div class="dd"><h2>Phone</h2><input type="phone" placeholder="Enter Phone"></div>
+        <div class="ff"><h2>Password</h2><input type="Password" >
+        </div>
+        <div class="ee"><h2>Confirm Password</h2><input type="Password" >
+        </div>
+        <div class="Gender"><h2>Gender</h2><input class="radio" type="radio" name="Gender"><p>male</p>
+        <input class="radio" type="radio" name="Gender"><p>female</p>
+        </div>
+        <div class="select">
+            <h2>Country</h2>
+            <select>
+                <option value="">USA</option>
+                <option value="">UA</option>
+                <option value="">Egypt</option>
+            </select>
+        </div>
+        <div class="select">
+            <h2>Date</h2>
+            <select>
+                <option value="">May</option>
+                <option value="">Marsh</option>
+                <option value="">junne</option>
+            </select>
+            <select>
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+            </select>
+            <select>
+                <option value="">1999</option>
+                <option value="">2001</option>
+                <option value="">2003</option>
+            </select>
+        </div>
+        <div class="Gender">
+        <input class="radio" type="radio" name="Gender"><p>I agree to the Terms of use</p>
+        </div>
+    </div>
+
+
+    <div class="B">
+    <input type="submit" value="submit" >
+    <input type="reset" value="Cancel" >
+    </div>
+</body>
+</html>
